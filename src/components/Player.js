9642808a39757b9614ded5e634b1cf3df77bd240
unknown <ahmedqayr22@gmail.com>
2021-06-12 16:33:18 +0100

    new

--- src/components/Player.js
+++ src/components/Player.js
@@ -2,8 +2,37 @@
 
 const Player = () => {
     return (
-        <div>
-           Players 
+        <div className="min-vh-100 shadow-lg p-3 bg-red">
+        <h1 className="text-center fs-5 fw-bold">Player</h1> <hr/>
+        <ul className="text-end list-group">
+            <div>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0 d-flex justify-content-between ">
+                     <span>Reciter:</span>
+                     <span className="fs-6 ">Abdil basit</span>
+                </li>
+                <hr/>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0 d-flex justify-content-between ">
+                     <span>Surah in Arabic:</span>
+                     <span className="fs-5 ">maida</span>
+                </li>
+                <hr/>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0 d-flex justify-content-between ">
+                     <span>Surah in English:</span>
+                     <span className="fs-6 ">Maida</span>
+                </li>
+                <hr/>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0 d-flex justify-content-between ">
+                     <span>Revalation Place:</span>
+                     <span className="fs-6 ">madinah</span>
+                </li>
+                <hr/>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0 d-flex justify-content-between ">
+                     <span>Translated Name:</span>
+                     <span className="fs-6 ">The Table </span>
+                </li>
+                <hr/>
+            </div>
+        </ul>
         </div>
     )
 }
--- src/components/Reciters.js
+++ src/components/Reciters.js
@@ -3,17 +3,29 @@ import { FaUserCircle } from 'react-icons/fa';
 const Reciters = () => {
     return (
         <div className="min-vh-100 shadow-lg p-3 bg-red">
-           <h1 className="text-center fs-5 fw-bold">Reciters</h1> <hr/>
-           <div>
-               <div className="d-flex aling-items-center py-0 cursor">
-                   <FaUserCircle className="fs-3" />{'  '}
-                   <span className="fs-5">Abdil basit</span>
-                  
-               </div>
-               
-               <hr />
-           </div>
+        <h1 className="text-center fs-5 fw-bold">Reciters</h1> <hr/>
+        <ul className="text-start list-group">
+            <div>
+                <li className="list-group-item bg-transparent border-0 text-light py-0  cursor fs-5 ps-0">
+                <FaUserCircle className="fs-3 me-3" />{'  '}
+                     <span className="fs-6 ">Abdil basit</span>
+                </li>
+                <hr/>
+            </div>
+        </ul>
         </div>
+    //     <div className="min-vh-100 shadow-lg p-3 bg-red">
+    //        <h1 className="text-center fs-5 fw-bold">Reciters</h1> <hr/>
+    //        <div>
+    //            <div className="d-flex aling-items-center py-0 cursor ">
+    //                <FaUserCircle className="fs-3" />{'  '}
+    //                <span className="fs-5 cursor2">Abdil basit</span>
+                  
+    //            </div>
+               
+    //            <hr  className="my-1" />
+    //        </div>
+    //     </div>
     )
 }
 
